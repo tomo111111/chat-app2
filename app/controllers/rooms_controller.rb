8f3a28ce2032354093f25dddb56c52fdff453097
tomo111111 <ttt0206sss@gmail.com>
2020-08-03 16:11:50 +0900

    チャット画面の編集

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,17 +4,17 @@ class RoomsController < ApplicationController
   end
 
   def create
-    @room = Room.create(room_params)
+   @room = Room.new(room_params)
     if @room.save
       redirect_to root_path
     else
-      rendaer :new
+      render :new
     end
   end
 
   private
 
   def room_params
-    params.require(:room).permit(:name,uesr_ids:[])
-    end
+    params.require(:room).permit(:name, user_ids:[])
+  end
 end
