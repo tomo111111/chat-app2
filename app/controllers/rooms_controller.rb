9e6f500d070bdece8a9f31193a53548c1195b135
tomo111111 <ttt0206sss@gmail.com>
2020-08-03 15:36:48 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,19 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+
+  def create
+    @room = Room.create(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      rendaer :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name,uesr_ids:[])
+    end
 end
